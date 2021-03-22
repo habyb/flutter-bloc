c3c6d9f5ead398d062fc32028b606188028af524
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-22 17:51:09 -0300

    name change screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'components/theme.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/counter.dart';
+import 'screens/name.dart';
 
 void main() {
   runApp(FlutterBloc());
+}
+
+class LogObserver extends BlocObserver{
+  @override
+    void onChange(Cubit cubit, Change change) {
+      print('${cubit.runtimeType} > $change');
+      super.onChange(cubit, change);
+    }
 }
 
 class FlutterBloc extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Bloc.observer = LogObserver();
     return MaterialApp(
       theme: bytebankTheme,
-      home: CounterContainer(),
+      home: NameContainer(),
     );
   }
 }
