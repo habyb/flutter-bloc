f35d409801864b65cb0e3a86759e6f7cb5fffbd4
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-22 16:09:40 -0300

    added theme component

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'components/theme.dart';
 import 'screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +11,7 @@ class FlutterBloc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ),
-      ),
+      theme: bytebankTheme,
       home: Dashboard(),
     );
   }
